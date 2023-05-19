9bf5001d402c22c059a4ed569b21c8b7ffec22e6
Z9 <linda2927@naver.com>
2023-05-19 22:56:10 +0900

    Feature/check oauth state param (#10)
    * feat: disable react strict mode
    
    * fix: set oauth2 state parameter to local storage and check state parameter on redirect uri

--- app/login/oauth2/code/[registrationId]/page.tsx
+++ app/login/oauth2/code/[registrationId]/page.tsx
@@ -1,18 +1,24 @@
 'use client';
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams, useRouter } from 'next/navigation';
 import authApi from '@apis/auth/authApi';
 
 export default function OAuth({ params }) {
+	console.log('oauth');
+	const router = useRouter();
 	const searchParams = useSearchParams();
 	const queryState = searchParams.get('state');
 	const code = searchParams.get('code');
 
 	const registrationId = params.registrationId;
-	const state = localStorage.getItem('state');
+	let state;
+	if (typeof window !== 'undefined') {
+		state = localStorage.getItem('state');
+	}
 	if (queryState === state) {
-		authApi
-			.createOAuthUser({ code, registrationId })
-			.then((r) => console.log(r));
+		authApi.createOAuthUser({ code, registrationId }).then((r) => {
+			console.log(r);
+			router.push('/main');
+		});
 	}
 
 	return null;
--- components/auth/LoginModal.tsx
+++ components/auth/LoginModal.tsx
@@ -1,5 +1,15 @@
 'use client';
 
+import { getKakaoAuthUri, setOAuth2State } from '@utils/oauth2';
+
+const state = setOAuth2State();
+const url = getKakaoAuthUri(state);
+
 export default function LoginModal() {
-	return <div>로그인 모달</div>;
+	return (
+		<div>
+			<p>로그인 모달</p>
+			<a href={url}>소셜 로그인</a>
+		</div>
+	);
 }
