4941a0f15277c12def7c630bd5e084b4ad54ffcd
linda2927 <linda2927@naver.com>
2023-05-20 15:15:43 +0900

    fix: make ModalWrapper accept custom height

--- components/layout/ModalWrapper.tsx
+++ components/layout/ModalWrapper.tsx
@@ -1,20 +1,30 @@
-interface ModalProps {
+type ModalProps = {
 	Modal: JSX.Element;
 	isModal: boolean;
 	onCloseModal: (any) => void;
-}
+	hasCloseButton?: boolean;
+	height: string;
+};
 
 export default function ModalWrapper({
 	Modal,
 	isModal,
 	onCloseModal,
+	hasCloseButton = true,
+	height,
 }: ModalProps) {
 	return (
 		isModal && (
-			<div className={'w-[50rem] h-[40rem]'}>
-				<button type={'button'} onClick={onCloseModal}>
-					닫기 버튼
-				</button>
+			<div
+				className={`w-[30rem] ${
+					height && `h-[${height}]`
+				} p-4 justify-items-center flex flex-col my-auto`}
+			>
+				{hasCloseButton && (
+					<button type={'button'} onClick={onCloseModal}>
+						닫기 버튼
+					</button>
+				)}
 				<Modal></Modal>
 			</div>
 		)
